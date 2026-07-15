8b5c4f27d8c9ef00254f45d2f18f4f1b34e954c9
AdsunRD <btduoc@adsun.vn>
2026-07-15 17:39:59 +0700

    Database setup

---
+++ VerticalSliceArchitectureWithCQRS/VerticalSliceArchitectureWithCQRS/Migrations/20260715103527_InitialCreate.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace VerticalSliceArchitectureWithCQRS.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    CustomerCode = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CustomerName = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Customers",
+                columns: new[] { "Id", "CustomerCode", "CustomerName" },
+                values: new object[,]
+                {
+                    { new Guid("0c30022b-8617-47ec-8e2d-6f327f507084"), "CUST-002", "Jane Smith" },
+                    { new Guid("e455f48f-35d1-4fa4-aaf1-4f7fcf5da22a"), "CUST-001", "John Doe" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
--- VerticalSliceArchitectureWithCQRS/VerticalSliceArchitectureWithCQRS/Program.cs
+++ VerticalSliceArchitectureWithCQRS/VerticalSliceArchitectureWithCQRS/Program.cs
@@ -1,23 +1,86 @@
+using Carter;
+using FluentValidation;
+using Microsoft.EntityFrameworkCore;
+using Scalar.AspNetCore;
+using VerticalSliceArchitectureWithCQRS.Data;
+using VerticalSliceArchitectureWithCQRS.Share.Behaviors;
+using VerticalSliceArchitectureWithCQRS.Share.CQRS;
+using VerticalSliceArchitectureWithCQRS.Share.Exceptions.Handler;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+var assembly = typeof(Program).Assembly;
+//instead of above, adding Scrutor 
+builder.Services.Scan(scan => scan.FromAssembliesOf(typeof(Program))
+    .AddClasses(classes => classes.AssignableTo(typeof(IQueryHandler<,>)), publicOnly: false)
+    .AsImplementedInterfaces()
+    .WithScopedLifetime()
+    .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+    .AsImplementedInterfaces()
+    .WithScopedLifetime()
+    .AddClasses(classes => classes.AssignableTo(typeof(ICommandHandler<,>)), publicOnly: false)
+    .AsImplementedInterfaces()
+    .WithScopedLifetime());
+builder.Services.AddScoped<IDispatcher, Dispatcher>();
 
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddValidatorsFromAssembly(assembly, includeInternalTypes: true);
+
+builder.Services.AddScoped(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+builder.Services.AddCarter();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
+});
+
+builder.Services.AddEndpointsApiExplorer();
+
 builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new() { Title = "Vertical Slice Architecture API", Version = "v1" });
+});
+builder.Services.AddLogging(loggingBuilder =>
+{
+    loggingBuilder.AddConsole();
+    loggingBuilder.AddDebug();
+});
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<CustomerContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+using (var scope = app.Services.CreateScope())
 {
-    app.MapOpenApi();
+    var db = scope.ServiceProvider.GetRequiredService<CustomerContext>();
+    db.Database.Migrate();
 }
+
+app.MapOpenApi();
+app.MapScalarApiReference();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Vertical Slice Architecture API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+app.UseCors("AllowAll");
+app.UseExceptionHandler(options => { });
 
-app.MapControllers();
+app.MapCarter();
 
 app.Run();
