37de343f3f2c0110f48ff7f1a1ffdece199541c5
AdsunRD <btduoc@adsun.vn>
2026-07-15 17:09:31 +0700

    DbContext

---
+++ VerticalSliceArchitectureWithCQRS/VerticalSliceArchitectureWithCQRS/Data/Extentions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace VerticalSliceArchitectureWithCQRS.Data
+{
+    public static class Extentions
+    {
+        public static IApplicationBuilder UseMigration(this IApplicationBuilder app)
+        {
+            using var scope = app.ApplicationServices.CreateScope();
+            using var dbContext = scope.ServiceProvider.GetRequiredService<CustomerContext>();
+
+            dbContext.Database.MigrateAsync();
+
+            return app;
+        }
+    }
+}
